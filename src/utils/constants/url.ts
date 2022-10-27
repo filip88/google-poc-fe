c0223903cbdeac08820ff79881d466af7ecd57a6
Veljko <veljkomiljkovic@live.com>
2022-10-27 18:19:28 +0200

    fix

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,5 +1,5 @@
 export const URL = {
   base: 'http://52.91.141.155:8080/api/v1/marketplaces?page=0&page-size=1000',
   delete: 'http://52.91.141.155:8080/api/v1/marketplaces',
-  mockapi: 'https://63593876c27556d2894eff39.mockapi.io/adverts'
+  mockapi: 'https://635aae536f97ae73a633d2b7.mockapi.io/google_poc'
 }
